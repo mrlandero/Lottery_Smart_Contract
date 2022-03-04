258cdad9d548843d7299f10785b8dc26d9bbb8f3
Tony <mr.landero@gmail.com>
2022-03-04 10:08:00 -0600

    Add entering the lottery section

--- lottery.sol
+++ lottery.sol
@@ -10,3 +10,10 @@ contract Lottery {
     constructor(){
         manager = msg.sender;
     }
+
+    // Entering the lottery
+    receive() external payable {  // In order for the contract to receive ETH - Must be included in contract
+        // Validation - The require statement
+        require(msg.value == 0.1 ether, "You can only send .1 ETH!"); // We can assign a suffix for the unit we are using (ether)
+        players.push(payable(msg.sender));  // Adding the player to the Players array
+    }
